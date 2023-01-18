5a79fdf3078ffe7d27c67d49f623edcae14b7044
javad14-gh <javad14396@gmail.com>
2023-01-18 15:48:54 +0300

    aaaa

--- linter.py
+++ linter.py
@@ -2,7 +2,8 @@ from telegram import Update , ReplyKeyboardMarkup ,MessageEntity
 from telegram.ext import ApplicationBuilder, ConversationHandler , CommandHandler , MessageHandler , ContextTypes,filters
 from openpyxl import load_workbook
 import pandas as pd
-
+from random import randint
+from datetime import date , timedelta
 
 menu = [['review','find word']]
 markup = ReplyKeyboardMarkup(menu,one_time_keyboard=True)
@@ -12,7 +13,7 @@ sheet = pd.read_excel(path)
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     name = update.message.from_user['first_name']
-    Entity = [{'offset':5,'length':len(name),'type':'bold'}]
+    Entity = [{'offset':6,'length':len(name),'type':'bold'}]
     await context.bot.send_sticker(chat_id=update.effective_chat.id, sticker='CAACAgQAAxkBAAEG_BhjpwzaE0_WGWvzgT9r-BK0Yy60GQACiwgAAmfHiVJJMa5s69pZ0iwE')
     await context.bot.send_message(chat_id= update.effective_chat.id, text=f'salam {name} joon khosh umadi\nomidvaram emruz kalamehaye khubi yad begirim',reply_markup=markup,entities=Entity)
     return menu
@@ -46,19 +47,25 @@ async def Error(update:Update,context:ContextTypes.DEFAULT_TYPE):
     return menu
 
 async def start_review(update: Update,context: ContextTypes.DEFAULT_TYPE):
-    sheet = load_workbook(path).active
-    myDict = {}
-    
-    for x in sheet:
-        myDict[x[0].value] = x[1].value
-        myDict[x[1].value] = x[0].value
-    
+    global review_word
+    def reviewWord():
+        w = sheet[sheet.date <= date.today()].kalame.get(randint(1,sheet.last_valid_index()))
+        return w
+    while True:
+        review_word = reviewWord()
+        if review_word:
+            await context.bot.send_message(chat_id=update.effective_chat.id,text=review_word)
+            mani = sheet[sheet.kalame == review_word].mani.item()
+            await context.bot.send_message(chat_id=update.effective_chat.id,text= mani ,entities=[{'offset':0,'length':len(mani),'type':'spoiler'}])
+            await context.bot.send_message(chat_id=update.effective_chat.id,text='balad budi?',reply_markup=ReplyKeyboardMarkup([['yes','no']],one_time_keyboard=True))
+            break    
     return isTrue
 
 async def get_mean(updat: Update, context:ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=updat.effective_chat.id,text='hala ke mikhay ezafe koni lotfan mani kalame ro be man bede')
     return addWord
 
+#TODO eslah beshe ba pandas
 async def add_word(update: Update,context:ContextTypes.DEFAULT_TYPE):
     mean = update.message.text.lower()
     wb = load_workbook(path)
@@ -74,6 +81,17 @@ async def return_menu(update:Update,context:ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id,text='bargashtim menuye asli hala chikar konim?????',reply_markup=markup)
     return menu
 
+async def check_review(update:Update,context:ContextTypes.DEFAULT_TYPE):
+    a = update.message.text
+    if a == 'yes':
+        status_list = [1,3,5,7,15,30]
+        i = sheet.index[sheet.kalame == review_word].item()
+        old_status = sheet[sheet.index == i].status.item()
+        sheet.at[i,'date'] +=  timedelta(days= old_status)
+        sheet.at[i,'status'] = status_list[status_list.index(old_status)+1]
+    await context.bot.send_message(chat_id=update.effective_chat.id,text='mikhay edame bedi?',reply_markup=ReplyKeyboardMarkup([['yes','no']],one_time_keyboard=True))
+    return menu
+
 def main():
     application = ApplicationBuilder().token('5024895126:AAGbQ7OBTxoiMxtdPtwATDNSzgY6Qlvcj7o').build()
     conv_handler = ConversationHandler(
@@ -85,7 +103,7 @@ def main():
             addOrNot: [MessageHandler(filters.Regex('^YES$'),get_mean),
                        MessageHandler(filters.Regex('^NO$'),return_menu)],
             addWord: [MessageHandler(filters.Regex('.*'),add_word)],
-            isTrue: []
+            isTrue: [MessageHandler(filters.Regex('.*'),check_review)]
         },
         fallbacks= []
     )
